ec5ff1f9885f789960b5389e21120bfa9fff7117
Mabelita <miespinozaco@gmail.com>
2023-10-15 22:03:09 +0200

    Scaffolding inicial Laravel + migrations + models

---
+++ backend-laravel/app/Models/Review.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'content',
+        'previous_book_status',
+    ];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
