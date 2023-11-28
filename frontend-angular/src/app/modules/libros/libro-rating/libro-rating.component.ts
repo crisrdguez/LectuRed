b6165bb5a63c7db58652814420eebc88716dc2a7
crisRR <140411058+crisrdguez@users.noreply.github.com>
2023-11-28 21:30:08 +0100

    componente rating modificaciones

--- frontend-angular/src/app/modules/libros/libro-rating/libro-rating.component.ts
+++ frontend-angular/src/app/modules/libros/libro-rating/libro-rating.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-libro-rating',
@@ -8,8 +8,53 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class LibroRatingComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { 
+  estadoActual = this.data.estadoLibro;
+  miCritica = this.data.critica;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog) { 
     console.log("Datos recibidos: ", data);
+  }
+
+  actualizarEstadoLibro(estado: string): void {
+    this.data.estadoLibro = estado;
+    this.estadoActual= estado;
+    console.log("Estado actualizado: ", this.data.estadoLibro);
+    
+    // Guardar en localStorage
+      const libroId = this.data.libroSeleccionado.id;
+      console.log("componente rating, el idLibro es: ", libroId);
+      const libroEstado = this.data.estadoLibro;
+      const libroPuntuacion = this.data.puntuacion;
+      const libroCritica = this.data.critica;
+      this.guardarLibroEnLocalStorage(libroId, libroEstado, libroPuntuacion, libroCritica);
+    
+    //TODO guardar en bbdd
+    //TODO ahora se pierden los datos cuando vuelvo al componente de mis libros, ya que obtiene los datos de un json y no de la bbdd. aqui se actualiza el localstorage con los datos de json, TIENE QUE ACTUALIZARSE CON LOS DE LA BBDD
+  }
+
+  actualizarLibroLocalStorage(libroId: string, estado: string, puntuacion: number, critica:string): void {
+    this.guardarLibroEnLocalStorage(libroId, estado, puntuacion, critica);
+    this.dialog.closeAll();
+  }
+
+  private guardarLibroEnLocalStorage(libroId: string, estado: string, puntuacion: number, critica:string): void {
+    const librosLocalStorage = JSON.parse(localStorage.getItem('misLibros') || '[]');
+
+    // Verificar si el libro ya está en localStorage
+    const libroExistente = librosLocalStorage.find((libro: any) => libro.idLibro === libroId);
+
+    if (libroExistente) {
+      // Actualizar el estado si ya existe en localStorage
+      libroExistente.estado = estado;
+      libroExistente.puntuacion = puntuacion;
+      libroExistente.critica = critica;
+    } else {
+      // Agregar el libro si no existe en localStorage
+      librosLocalStorage.push({ idLibro: libroId, estado, puntuacion, critica });
+    }
+
+    // Guardar en localStorage
+    localStorage.setItem('misLibros', JSON.stringify(librosLocalStorage));
   }
 
 }
