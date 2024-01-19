4c7980edb788a492564e90cbe88d08a295c40583
Mabelita <miespinozaco@gmail.com>
2024-01-19 18:44:16 +0100

    Sustitución de Factories por Seeders con datos concretos.

--- backend-laravel/database/seeders/ActividadSeeder.php
+++ backend-laravel/database/seeders/ActividadSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\MiLista;
-use App\Models\Actividad;
+use App\Models\ActividadMiLista;
+
 
 class ActividadSeeder extends Seeder
 {
@@ -15,6 +16,89 @@ class ActividadSeeder extends Seeder
      */
     public function run(): void
     {
-        Actividad::factory()->count(100)->create();
+        ActividadMiLista::create([
+            'user_id' => 1,
+            'nombre' => 'Noa',
+            'idlibro' => 'hWSmEAAAQBAJ',
+            'puntuacion' => 5,
+            'critica' => 'El mejor libro del mundo...',
+            'estado' => 'leido',
+            'created_at' => '2024-01-19 17:01:45',
+            'updated_at' => '2024-01-19 17:01:45',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 2,
+            'nombre' => 'Daniel',
+            'idlibro' => 'YZseCgAAQBAJ',
+            'puntuacion' => 0,
+            'estado' => 'leyendo',
+            'created_at' => '2024-01-19 17:02:33',
+            'updated_at' => '2024-01-19 17:02:33',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 3,
+            'nombre' => 'Samuel',
+            'idlibro' => 'DDHFEAAAQBAJ',
+            'puntuacion' => 0,
+            'estado' => 'deseado',
+            'created_at' => '2024-01-19 17:03:05',
+            'updated_at' => '2024-01-19 17:03:05',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 4,
+            'nombre' => 'Eduardo',
+            'idlibro' => 't1slEAAAQBAJ',
+            'puntuacion' => 5,
+            'critica' => 'Una joya literaria que cautiva desde la primera página...',
+            'estado' => 'leido',
+            'created_at' => '2024-01-19 17:04:00',
+            'updated_at' => '2024-01-19 17:04:00',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 5,
+            'nombre' => 'Ivan',
+            'idlibro' => 'LsBkzwEACAAJ',
+            'puntuacion' => 0,
+            'estado' => 'leyendo',
+            'created_at' => '2024-01-19 17:04:37',
+            'updated_at' => '2024-01-19 17:04:37',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 1,
+            'nombre' => 'Noa',
+            'idlibro' => 'YZseCgAAQBAJ',
+            'puntuacion' => 4,
+            'critica' => 'Este libro es una obra de arte literaria...',
+            'estado' => 'leido',
+            'created_at' => '2024-01-19 17:06:04',
+            'updated_at' => '2024-01-19 17:06:04',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 2,
+            'nombre' => 'Daniel',
+            'idlibro' => 'LsBkzwEACAAJ',
+            'puntuacion' => 4,
+            'critica' => 'Este libro es un tesoro literario...',
+            'estado' => 'leido',
+            'created_at' => '2024-01-19 17:11:42',
+            'updated_at' => '2024-01-19 17:11:42',
+        ]);
+    
+        ActividadMiLista::create([
+            'user_id' => 3,
+            'nombre' => 'Samuel',
+            'idlibro' => 'LsBkzwEACAAJ',
+            'puntuacion' => 5,
+            'critica' => 'Un triunfo literario que deja una impresión imborrable...',
+            'estado' => 'leido',
+            'created_at' => '2024-01-19 17:12:51',
+            'updated_at' => '2024-01-19 17:12:51',
+        ]);
     }
 }
--- backend-laravel/database/seeders/UserSeeder.php
+++ backend-laravel/database/seeders/UserSeeder.php
@@ -11,8 +11,67 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        User::factory()->count(25)->create();            
-    }
+
+public function run(): void
+{
+    User::create([
+        'name' => 'Noa',
+        'last_name' => 'Espinoza',
+        'birth_date' => '1986-04-27',
+        'email' => 'noa@gmail.com',
+        'email_verified_at' => '2024-01-18 18:24:39',
+        'password' => '$2y$10$l/YAuGGuCbilOiBK55C0f.EeowaP9IiMZBOUXqk2bE9U8ops/uU5S',
+        'city' => 'Alcalá de Henarés',
+        'created_at' => '2024-01-18 16:33:43',
+        'updated_at' => '2024-01-18 21:49:45',
+    ]);
+
+    User::create([
+        'name' => 'Daniel',
+        'last_name' => 'del Valle',
+        'birth_date' => '1971-09-08',
+        'email' => 'daniel@gmail.com',
+        'email_verified_at' => '2024-01-18 18:24:40',
+        'password' => '$2y$10$l/YAuGGuCbilOiBK55C0f.EeowaP9IiMZBOUXqk2bE9U8ops/uU5S',
+        'city' => 'Talavera de la Reina',
+        'created_at' => '2024-01-18 16:48:54',
+        'updated_at' => '2024-01-18 16:48:54',
+    ]);
+
+    User::create([
+        'name' => 'Samuel',
+        'last_name' => 'Velasco',
+        'birth_date' => '2024-01-18',
+        'email' => 'samuel@gmail.com',
+        'email_verified_at' => '2024-01-18 18:24:41',
+        'password' => '$2y$10$f/xwBM1MdiYfh3olWVfkpOamxVXwU4u.nBrDhEIQLrZpslcgAQ0PK',
+        'city' => 'Granada',
+        'created_at' => '2024-01-18 16:55:31',
+        'updated_at' => '2024-01-18 21:58:27',
+    ]);
+
+    User::create([
+        'name' => 'Eduardo',
+        'last_name' => 'Andrade',
+        'birth_date' => '1947-09-19',
+        'email' => 'eduardo@gmail.com',
+        'email_verified_at' => '2024-01-18 18:24:42',
+        'password' => '$2y$10$hAjMAazdWrUEC0ODV5hjZ.FtnPeD.tMpzpV0zwkAbF.arT5zF4fMG',
+        'city' => 'San Sebastian de los Reyes',
+        'created_at' => '2024-01-18 16:59:32',
+        'updated_at' => '2024-01-18 16:59:32',
+    ]);
+
+    User::create([
+        'name' => 'Ivan',
+        'last_name' => 'Rodriguez',
+        'birth_date' => '2000-01-18',
+        'email' => 'ivan@gmail.com',
+        'email_verified_at' => '2024-01-18 18:24:44',
+        'password' => '$2y$10$ghLgOXE1oFcpJ/siXH1ZkOeP8VsXEphPax8xjMRS9RW7TtXyzkKOu',
+        'city' => 'Barcelona',
+        'created_at' => '2024-01-18 17:01:40',
+        'updated_at' => '2024-01-18 17:01:40',
+    ]);
+}
 }
